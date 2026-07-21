ac446a984f21f84ecbc6ee720e5b36d267cde6d8
Max <maxthacker1@gmail.com>
2026-07-21 13:33:58 -0700

    1-8 done

--- MGT6203_HW5_2.R
+++ MGT6203_HW5_2.R
@@ -9,18 +9,35 @@ mydata <- mydata[,-c(1,8)]
 mydata[,-7] <- scale(mydata[,-7])
 
 # 3. Split into test and train datasets
-
-
+n.train <- floor(nrow(mydata)*0.8)
+set.seed(1000)
+ind.train<- sample(1:nrow(mydata),n.train)
+data.train <- mydata[ind.train,]
+data.test <- mydata[-ind.train,]
 
 # 4. Fit simple neural network
+nn <- neuralnet(Up ~ Lag1 + Lag2, data = data.train, hidden = 2 , linear.output = FALSE )
+load("Smarket_nn1.Rda")
 
 # 5. Plot Neural network
+plot(nn, rep="best")
 
 # 6. display weights of neural network
+nn$weights
 
-# 7. Manually cacluate the values of activation and output nodes
+# 7. Manually calculate the values of activation and output nodes
+data.test[1,]
+s1 <- (-1.5526810)+(-0.4006151)*(-0.5516457)+(0.3979986)*(0.9047775)
+s1 <- exp(s1)/(1+exp(s1))
+
+s2 <- (-20.092955)+(1.325688)*(-0.5516457)+(33.606746)*(0.9047775)
+s2 <- exp(s2)/(1+exp(s2))
+
+p1 <- (-0.2670554) + (2.5133068)*s1+(-0.7918837)*s2
+p1 <- exp(p1)/(1+exp(p1))
 
 # 8. Verify manual calculations with compute
+compute(nn, data.test[1,])
 
 # 9. Create more complex NN
 
